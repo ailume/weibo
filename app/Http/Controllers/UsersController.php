108590bb3a4f08304af5457d105790e865410a8d
ailume <ailume@163.com>
2021-03-13 13:43:33 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -11,4 +12,19 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+
+    /**
+     *
+     * @User: w
+     * @Date: 2021/3/13
+     * @Time: 下午9:31
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
 }
