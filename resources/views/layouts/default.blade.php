287a8ce2b3895d0fa774d2310eaca240cec84376
ailume <ailume@163.com>
2021-03-13 09:14:45 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
@@ -16,7 +16,8 @@
     </div>
 </nav>
 
-@yield('content')
-
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
