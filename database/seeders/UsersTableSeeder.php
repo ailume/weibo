c48f2a00381423f5a5ce67094272f74967a4fba4
ailume <ailume@163.com>
2021-03-15 08:59:08 +0000

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -10,10 +10,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::factory()->count(50)->create();
-
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
