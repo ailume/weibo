e28ed1f307d7440652ab6ab73d76097caf1089ae
ailume <ailume@163.com>
2021-09-17 14:19:57 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,16 +7,35 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    // 创建用户页面
+    // 创建用户注册页面
     public function create()
     {
         return view('users.create');
     }
 
 
+    /**
+     * 显示用户数据
+     * @User: w
+     * @Date: 2021/9/17
+     * @Time: 下午10:15
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 
 
