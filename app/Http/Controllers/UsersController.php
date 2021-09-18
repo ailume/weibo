bc4276915356915ec4cd75923c65a9f33ed6831f
ailume <ailume@163.com>
2021-09-18 01:34:23 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -55,4 +55,47 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+
+    /**
+     * 编辑页面
+     * @User: w
+     * @Date: 2021/9/18
+     * @Time: 上午9:18
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+
+    /**
+     * 用户更新数据
+     * @User: w
+     * @Date: 2021/9/18
+     * @Time: 上午9:31
+     * @param User $user
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user);
+    }
+
+
+
 }
